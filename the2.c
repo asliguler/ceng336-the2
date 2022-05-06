aedfc17ec2990f23477e4788eaf4b280057b8951
asliguler <95230116+asliguler@users.noreply.github.com>
2022-05-06 17:48:35 +0300

    Update the2.c

--- the2.c
+++ the2.c
@@ -4,13 +4,17 @@
 void tmr0_isr();
 void __interrupt(high_priority) highPriorityISR(void) {
     if (INTCONbits.TMR0IF) tmr0_isr();
-    else if (PIR1bits.TMR1IF) tmr1_isr();
+    else if (PIR1bits.TMR1IF) tmr1_isr(); // TODO: can be also low priorty TBD
 }
 void __interrupt(low_priority) lowPriorityISR(void) {}
 
 uint8_t game_started = 0, game_level = 1;
 
 uint8_t health = 9;
+
+bool display_side = 0; 
+
+uint8_t rc0_state = 0;
 
 typedef enum {TMR_IDLE, TMR_RUN, TMR_DONE} tmr_state_t;
 tmr_state_t tmr0_state = TMR_IDLE, tmr1_state = TMR_IDLE;
@@ -36,6 +40,7 @@ void init_ports() {
 }
 
 void init_irq() {
+    // global & timer0 & timer1 interruptlarını enable et
     INTCONbits.TMR0IE = 1;
     PIE1.TMR1IE = 1;
     INTCONbits.GIE = 1;
@@ -46,9 +51,11 @@ void config_tmr0() {
     T0CONbits.T0PS0 = 1; // 1:256 pre-scaler
     T0CONbits.T0PS1 = 1; // 1:256 pre-scaler
     T0CONbits.T0PS2 = 1; // 1:256 pre-scaler 
+    // INFO: Timer0'nun degerlerini TMR0L registerindan oku
 }
 
 void tmr0_preload() {
+    // timer0'nun baslangic degerleri
     switch(game_level) {
         case 1:
             TMR0L = TIMER0_PRELOAD_LEVEL1 & 0xff;
@@ -60,9 +67,12 @@ void tmr0_preload() {
 }
 
 void tmr0_isr(){
+    // interrupt flagini disable edelim ki 
+    // tekrar interrupt gelmis gibi algilamasin
     INTCONbits.TMR0IF = 0;
     switch (game_level) {
         case 1:
+            // 500 msec tamamlamak icin 76 kere overflow olmali
             if tmr0_count < 76 {
                 tmr0_count++;
                 TMR0L = 0x00;
@@ -73,6 +83,7 @@ void tmr0_isr(){
                 tmr0_preload();
             }
         case 2:
+            // 400 msec tamamlamak icin 61 kere overflow olmali
             if tmr0_count < 61 {
                 tmr0_count++;
                 TMR0L = 0x00;
@@ -83,6 +94,7 @@ void tmr0_isr(){
                 tmr0_preload();
             }
         case 3:
+            // 300 msec tamamlamak icin 45 kere overflow olmali
             if tmr0_count < 45 {
                 tmr0_count++;
                 TMR0L = 0x00;
@@ -96,16 +108,18 @@ void tmr0_isr(){
 }
 
 void tmr1_isr() {
+    // interrupt flagini disable edelim ki 
+    // tekrar interrupt gelmis gibi algilamasin
     PIR1bits.TMR1IF = 0;
+    // overflow oldugu icin timer tamamlandi
     tmr1_state = TMR_DONE;
 }
 
 void init_tmr1() {
-    if (game_started) {
-        T1CON = 0x00;
-    }
-    // read value of TIMER1 from TMR1H:TMR1L registers
+    T1CON = 0x00;
+    // INFO: read value of TIMER1 from TMR1H:TMR1L registers
 }
+
 
 uint16_t generate_random() {
     uint16_t random_note =  (TMR1H << 8) | TMR1L;
@@ -122,20 +136,70 @@ uint16_t generate_random() {
 }
 
 
-void initial_7segment_display() {
-    // !!TODO: THIS SHOULD BE INSIDE A LOOP TO SWITCH AND FORWARD
-    //         BETWEEN LEFTMOST AND RIGHTMOST DISPLAYS
+void seven_segment_display() {
     
-    // set health to 9
-    PORTH = 0x01; // to set leftmost display
-    PORTJ = 0x6f; // to set 9
-    // TODO: wait a while
-    // set level to 1
-    PORTH = 0x08; // to set rightmost display
-    PORTJ = 0x06; // to set 1
+    if (game_started){
+        if (tmr1_state == TMR_DONE) { 
+            display_side != display_side
+            tmr1_state = TMR_IDLE;
+            tmr1_startreq = 1;
+        }
+        if (display_side) {
+            PORTH = 0x01; // to set leftmost display
+            // set health
+            switch (health) {
+                case 9:
+                    PORTJ = 0x6f; // to set 9
+                    break;
+                case 8:
+                    PORTJ = 0x7f; 
+                    break;
+                case 7:
+                    PORTJ = 0x07;
+                    break;
+                case 6:
+                    PORTJ = 0x7d;
+                    break;
+                case 5:
+                    PORTJ = 0x6d;
+                    break;
+                case 4:
+                    PORTJ = 0x66;
+                    break;
+                case 3:
+                    PORTJ = 0x4f;
+                    break;
+                case 2: 
+                    PORTJ = 0x5b;
+                    break;
+                case 1:
+                    PORTJ = 0x06;
+                    break;
+                case 0: 
+                    PORTJ = 0x3f;
+                    break;
+            }
+            
+            
+        } else {
+            PORTH = 0x08; // to set rightmost display
+            switch (game_level) {
+                case 3:
+                    PORTJ = 0x4f;
+                    break;
+                case 2: 
+                    PORTJ = 0x5b;
+                    break;
+                case 1:
+                    PORTJ = 0x06;
+                    break;
+            }
+        }
+    }
+
 }
 
-void timer0_task() {
+void timer_task() {
     switch (tmr0_state) {
         case TMR_IDLE:
             if (tmr0_startreq) {
@@ -156,13 +220,15 @@ void timer0_task() {
             if (tmr1_startreq) {
                 tmr1_startreq = 0;
                 T1CONbits.TMR1ON = 1;
-                tmr1_state TMR_RUN;
+                tmr1_state = TMR_RUN;
             }
     }
 }
 
 void input_task() {
-    if (PORTCbits.RC0) {
+    if (PORTCbits.RC0) rc0_state = 1;
+    else if (rc0_state == 1){
+        rc0_state = 0;
         game_started = 1;
         TRISC = 0x00;
         game_level = 1;
@@ -183,10 +249,11 @@ void game_over() {
 void main(void) {
     init_ports();
     config_tmr0();
+    init_tmr1();
     init_irq();
     while(1) {
+        seven_segment_display();
         input_task();
-        init_tmr1();
         timer0_task();
         game_task();
     }
