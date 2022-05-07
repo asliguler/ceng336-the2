8282df94caef4ce8e441c5b92d0048f347c0bc56
Ahmet Kaan Akgüloğlu <48061204+KaanAkguloglu@users.noreply.github.com>
2022-05-07 20:27:12 +0300

    Ellen MERGED

--- the2.c
+++ the2.c
@@ -1,20 +1,21 @@
 #include <xc.h>
 #include <stdint.h>
+#pragma config OSC = HSPLL
 
 void tmr0_isr();
-void __interrupt(high_priority) highPriorityISR(void) {
+void tmr1_isr();
+void __interrupt() highPriorityISR(void) {
     if (INTCONbits.TMR0IF) tmr0_isr();
-    else if (PIR1bits.TMR1IF) tmr1_isr(); // TODO: can be also low priorty TBD
+    else if (PIR1bits.TMR1IF) tmr1_isr();
+    return;// TODO: can be also low priorty TBD
 }
-void __interrupt(low_priority) lowPriorityISR(void) {}
+
 
 uint8_t game_started = 0, game_level = 1;
 
 uint8_t health = 9;
 
-bool display_side = 0; 
-
-uint8_t rc0_state = 0;
+uint8_t display_side = 0;
 
 typedef enum {TMR_IDLE, TMR_RUN, TMR_DONE} tmr_state_t;
 tmr_state_t tmr0_state = TMR_IDLE, tmr1_state = TMR_IDLE;
@@ -22,24 +23,30 @@ uint8_t tmr0_startreq = 0, tmr1_startreq = 0;
 uint8_t tmr0_ticks_left, tmr1_ticks_left; 
 uint8_t tmr0_count = 0;
 
-uint16_t random_n_value;
+uint16_t random_n_value = 0;
 
 
 typedef enum { LVL_BEG, LVL_CONT, LVL_BLANK, LVL_END} level_state_t;
 level_state_t level_state = LVL_BEG; 
+
+typedef enum {GAME_OVER_END, GAME_OVER_LOSE, GAME_OVER_DEFAULT} game_over_state_t;
+game_over_state_t game_over_state = GAME_OVER_DEFAULT; 
+
+// press_released   :   High bits ->  Press,    Low bits -> Release
+uint8_t press_released = 0x00;  
+uint8_t successfull_press = 0 ;
+
+typedef enum {NOT_PRSS, PRSS } press_state_t; 
+press_state_t press_state0=NOT_PRSS, press_state1=NOT_PRSS, press_state2=NOT_PRSS , press_state3=NOT_PRSS , press_state4=NOT_PRSS , press_state5=NOT_PRSS;
+press_state_t press_state_rc0 = NOT_PRSS;
 
 
 uint8_t level_max_note = 5; 
 uint8_t note_count = 0; 
 uint8_t blank_note_count = 0; // Will be used for counting 5 times when we are forwarding after note_count reached its max
 
-
-typedef enum {NOT_PRSS, PRSS } press_state_t; 
-press_state_t press_state0=NOT_PRSS, press_state1=NOT_PRSS, press_state2=NOT_PRSS , press_state3=NOT_PRSS , press_state4=NOT_PRSS , press_state5=NOT_PRSS;
-
 uint8_t miss_penalty = 1;
 uint8_t correct_note;
-
 
 #define TIMER0_PRELOAD_LEVEL1 74
 #define TIMER0_PRELOAD_LEVEL2 4
@@ -56,23 +63,44 @@ void init_ports() {
     TRISG = 0x1f;
     TRISH = 0x00;
     TRISJ = 0x00;
-
+    
+    
     LATA = 0x00;
     LATB = 0x00;
     LATC = 0x00;
+    PORTC = 0x00;
     LATD = 0x00;
     LATE = 0x00;
     LATF = 0x00;
     LATG = 0x00;
+    PORTG = 0x00;
     LATH = 0x00;
     LATJ = 0x00;
-    
 }
 
-void init_irq() { //init IRAQ
-    // global & timer0 & timer1 interruptlarını enable et
+void init_variables(){
+    health = 9;
+    game_level = 1;
+    press_state_rc0 = NOT_PRSS;
+    press_state0=NOT_PRSS; 
+    press_state1=NOT_PRSS; 
+    press_state2=NOT_PRSS; 
+    press_state3=NOT_PRSS;
+    press_state4=NOT_PRSS;
+    press_state5=NOT_PRSS;
+    tmr0_count = 0;
+    tmr0_startreq = 0; 
+    tmr1_startreq = 0;
+    level_max_note = 5; 
+    note_count = 0; 
+    blank_note_count = 0;
+}
+
+void init_irq() {
+    // global & timer0 & timer1 interruptlarini enable et
     INTCONbits.TMR0IE = 1;
-    PIE1.TMR1IE = 1;
+    INTCONbits.PEIE = 1;
+    PIE1bits.TMR1IE = 1;
     INTCONbits.GIE = 1;
 }
 
@@ -80,7 +108,7 @@ void config_tmr0() {
     T0CONbits.T08BIT = 1; // 8-bit mode
     T0CONbits.T0PS0 = 1; // 1:256 pre-scaler
     T0CONbits.T0PS1 = 1; // 1:256 pre-scaler
-    T0CONbits.T0PS2 = 1; // 1:256 pre-scaler 
+    T0CONbits.T0PS2 = 1; // 1:256 pre-scaler
     // INFO: Timer0'nun degerlerini TMR0L registerindan oku
 }
 
@@ -103,7 +131,7 @@ void tmr0_isr(){
     switch (game_level) {
         case 1:
             // 500 msec tamamlamak icin 76 kere overflow olmali
-            if tmr0_count < 76 {
+            if (tmr0_count < 76) {
                 tmr0_count++;
                 TMR0L = 0x00;
             }
@@ -112,10 +140,9 @@ void tmr0_isr(){
                 tmr0_state = TMR_DONE;
                 tmr0_preload();
             }
-            break;
         case 2:
             // 400 msec tamamlamak icin 61 kere overflow olmali
-            if tmr0_count < 61 {
+            if (tmr0_count < 61) {
                 tmr0_count++;
                 TMR0L = 0x00;
             }
@@ -124,10 +151,9 @@ void tmr0_isr(){
                 tmr0_state = TMR_DONE;
                 tmr0_preload();
             }
-            break;
         case 3:
             // 300 msec tamamlamak icin 45 kere overflow olmali
-            if tmr0_count < 45 {
+            if (tmr0_count < 45) {
                 tmr0_count++;
                 TMR0L = 0x00;
             }
@@ -136,7 +162,6 @@ void tmr0_isr(){
                 tmr0_state = TMR_DONE;
                 tmr0_preload();
             }
-            break;
     }
 }
 
@@ -146,26 +171,28 @@ void tmr1_isr() {
     PIR1bits.TMR1IF = 0;
     // overflow oldugu icin timer tamamlandi
     tmr1_state = TMR_DONE;
+    TMR1L = 0x00;
+    TMR1H = 0x00;
 }
 
 void init_tmr1() {
     T1CON = 0x00;
+    PIR1bits.TMR1IF = 0;
+    TMR1L = 0x00;
+    TMR1H = 0x00;
     // INFO: read value of TIMER1 from TMR1H:TMR1L registers
 }
 
 
-uint16_t generate_random() {
+uint16_t generate_random_note() {
     uint16_t random_note =  (TMR1H << 8) | TMR1L;
     switch(game_level) {
         case 1:
             random_note = ((random_note >> 1) & 0x0007);
-            break;
         case 2:
             random_note = ((random_note >> 3) & 0x0007);
-            break;
         case 3:
             random_note = ((random_note >> 5) & 0x0007);
-            break;
     }
     random_note %= 5;
     return random_note;
@@ -173,10 +200,10 @@ uint16_t generate_random() {
 
 
 void seven_segment_display() {
-    
-    if (game_started){
+    if (game_over_state == GAME_OVER_DEFAULT){
         if (tmr1_state == TMR_DONE) { 
-            display_side != display_side
+            if(display_side == 1) display_side = 0;
+            else display_side = 1;
             tmr1_state = TMR_IDLE;
             tmr1_startreq = 1;
         }
@@ -231,11 +258,56 @@ void seven_segment_display() {
                     break;
             }
         }
+    } else if (game_over_state == GAME_OVER_END) {
+        if (tmr1_state == TMR_DONE) { 
+            if (display_side < 3) display_side++;
+            else display_side = 0;
+            tmr1_state = TMR_IDLE;
+            tmr1_startreq = 1;
+        }
+        switch (display_side) {
+            case 0:
+                PORTH = 0x01;
+                PORTJ = 0x79;
+                break;
+            case 1:
+                PORTH = 0x02;
+                PORTJ = 0x54;
+                break;
+            case 2:
+                PORTH = 0x04;
+                PORTJ = 0x5e;
+                break;
+        }
+    } else if (game_over_state == GAME_OVER_LOSE) {
+        if (tmr1_state == TMR_DONE) { 
+            if (display_side < 4) display_side++;
+            else display_side = 0;
+            tmr1_state = TMR_IDLE;
+            tmr1_startreq = 1;
+        }
+        switch (display_side) {
+            case 0:
+                PORTH = 0x01;
+                PORTJ = 0x38;
+                break;
+            case 1:
+                PORTH = 0x02;
+                PORTJ = 0x3f;
+                break;
+            case 2:
+                PORTH = 0x04;
+                PORTJ = 0x6d;
+                break;
+            case 3:
+                PORTH = 0x08;
+                PORTJ = 0x79;
+                break;
+        }
     }
-
 }
 
-void timer0_task() {
+void timer_task() {
     switch (tmr0_state) {
         case TMR_IDLE:
             if (tmr0_startreq) {
@@ -246,6 +318,7 @@ void timer0_task() {
                 T0CONbits.TMR0ON = 1;
                 tmr0_state = TMR_RUN;
             }
+            break;
         case TMR_RUN:
             break;
         case TMR_DONE:
@@ -256,9 +329,12 @@ void timer0_task() {
         case TMR_IDLE:
             if (tmr1_startreq) {
                 tmr1_startreq = 0;
+                TMR1L = 0x00;
+                TMR1H = 0x00;
                 T1CONbits.TMR1ON = 1;
                 tmr1_state = TMR_RUN;
             }
+            break;
         case TMR_RUN:
             break;
         case TMR_DONE:
@@ -267,12 +343,16 @@ void timer0_task() {
 }
 
 void input_task() {
-    if (PORTCbits.RC0) rc0_state = 1;
-    else if (rc0_state == 1){
-        rc0_state = 0;
-        game_started = 1;
-        TRISC = 0x00;
-        game_level = 1;
+    if (game_started == 0){    
+        if (PORTCbits.RC0) press_state_rc0 = PRSS; // RC0'ye basildi
+        else if (press_state_rc0 == PRSS){ // RC0 release edildi
+            press_state_rc0 = NOT_PRSS;
+            game_started = 1; 
+            TRISC = 0x00; // RC0'yu bundan sonra notlari gostermek icin output olarak kullanacagiz
+            game_level = 1;
+            health = 9;
+            game_over_state = GAME_OVER_DEFAULT;
+        }
     }
 }
 
@@ -286,10 +366,6 @@ void forward_task(){
     LATC = LATB;
     LATB = LATA;
 }
-
-
-// 
-
 
 void check_press_task(){
     // LATG0
@@ -384,75 +460,63 @@ void check_press_task(){
 }
 
 void note_task(){
-    tmr0_startreq = 1;
-    uint16_t random_note =  create_note_task();
-
+    uint16_t random_note = generate_random_note();
     miss_penalty = 0;
     correct_note = random_note;
-    
     switch(random_note){
         case 0 :
             LATA = 0x01;
-            
             break;
         case 1 :
             LATA = 0x02;
-            
             break;
         case 2 :
             LATA = 0x04;
-             
             break;
         case 3 :
             LATA = 0x08;
-             
             break;
         case 4 :
             LATA = 0x10;
-             
             break;
     }
 }
-
-
 
 void game_task() {
     if(game_started){
         switch(level_state){
             case LVL_BEG :
                 if(tmr0_state == TMR_IDLE){
-                    tmr0_startreq = 1;rc0_state
+                    tmr0_startreq = 1;
+                    note_task();
+                    note_count++;
+                    level_state = LVL_CONT;
                 }
                 break;
 
             case LVL_CONT:
                 switch (tmr0_state){
                     case TMR_IDLE:
-                        
                         if(note_count != level_max_note){
                             tmr0_startreq = 1;
                             note_count++;
                             forward_task();
                             note_task();
-                            
                         }
                         else{
                             level_state = LVL_BLANK;
                         }
 
                         break;
-
                     case TMR_RUN:
                         if(note_count>5){
                             check_press_task();
                         }
                         break;
-
                     case TMR_DONE:
                         break;
-
                 }
-            break;
+                break;
 
             case LVL_BLANK:
                 switch (tmr0_state){
@@ -485,6 +549,7 @@ void game_task() {
                 
                 if(++game_level > 3){
                     level_max_note = 5;
+                    game_over_state = GAME_OVER_END;
                     game_over(); // END_GAME(), WE ARE IN THE END GAME NOW. 
                 }
                 
@@ -492,25 +557,25 @@ void game_task() {
                 break;
         }
     }
-
+}
 
 void game_over() {
-    //TODO: appropriate texts displayed on 7-segment display
-    TRISC = 0x01;
     game_started = 0;
-    health = 9;
+    init_ports();
+    init_variables();
 }
 
 void main(void) {
     init_ports();
+    init_variables();
     config_tmr0();
     init_tmr1();
     init_irq();
+    tmr1_startreq = 1;
     while(1) {
         seven_segment_display();
         input_task();
-        timer0_task();
+        timer_task();
         game_task();
     }
 }
-
