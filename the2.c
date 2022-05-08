4b0d5ce8fb8ca1e5952bbad22d1fcedca02cd596
Ahmet Kaan Akgüloğlu <48061204+KaanAkguloglu@users.noreply.github.com>
2022-05-08 14:34:54 +0300

    Comments and Fix on correct note logic.
    Added comments.
    Fixed correct note logic by adding get_correct_note() function.

--- the2.c
+++ the2.c
@@ -15,38 +15,47 @@ uint8_t game_started = 0, game_level = 1;
 
 uint8_t health = 9;
 
-uint8_t display_side = 0;
+uint8_t display_side = 0; // Used for 7 display segment that is alternating with the timer1. 
 
-typedef enum {TMR_IDLE, TMR_RUN, TMR_DONE} tmr_state_t;
+//Timer states used for both timers.
+//Timer waits for a request while TMR_IDLE. 
+//After the request state becomes TMR_RUN and stays as for a period according to level (500ms for level1, 400ms for level2). 
+//After the period, it becomes TMR_DONE. 
+typedef enum {TMR_IDLE, TMR_RUN, TMR_DONE} tmr_state_t;  
 tmr_state_t tmr0_state = TMR_IDLE, tmr1_state = TMR_IDLE;
-uint8_t tmr0_startreq = 0, tmr1_startreq = 0;
+
+uint8_t tmr0_startreq = 0, tmr1_startreq = 0; //Requests for timers. 
 uint8_t tmr0_ticks_left, tmr1_ticks_left; 
 uint8_t tmr0_count = 0;
 
-uint16_t random_n_value = 0;
+uint16_t random_n_value = 0; //N value that is used for the random note generation algorithm. 
 
-
+//Level states used for the levels. Gives information about which level state is a level in (should we light notes or blank notes, did the level finish etc.). 
+// LVL_BEG is the beginning of a level, where we light first note.
+// LVL_CONT is the states where we light all the other notes. (we will have 4 for level 1, 9 for level 2, 14 for level 3).
+// LVL_END is the end state of a level.
 typedef enum { LVL_BEG, LVL_CONT, LVL_BLANK, LVL_END} level_state_t;
 level_state_t level_state = LVL_BEG; 
 
+
+// TODO: Commment 
 typedef enum {GAME_OVER_END, GAME_OVER_LOSE, GAME_OVER_DEFAULT} game_over_state_t;
 game_over_state_t game_over_state = GAME_OVER_DEFAULT; 
 
-// press_released   :   High bits ->  Press,    Low bits -> Release
-uint8_t press_released = 0x00;  
-uint8_t successfull_press = 0 ;
-
+//Press states for five buttons that user will use :  RG0, RG1, RG2, RG3, RG4. 
 typedef enum {NOT_PRSS, PRSS } press_state_t; 
 press_state_t press_state0=NOT_PRSS, press_state1=NOT_PRSS, press_state2=NOT_PRSS , press_state3=NOT_PRSS , press_state4=NOT_PRSS , press_state5=NOT_PRSS;
 press_state_t press_state_rc0 = NOT_PRSS;
 
 
-uint8_t level_max_note = 5; 
-uint8_t note_count = 0; 
-uint8_t blank_note_count = 0; // Will be used for counting 5 times when we are forwarding after note_count reached its max
+uint8_t level_max_note = 5;  //Number of notes in a level. 
+uint8_t note_count = 0;     //Note count integer.
 
-uint8_t miss_penalty = 1;
-uint8_t correct_note;
+// Blank note counter integer. Will be used for counting 5 times when we are forwarding after note_count reached its max. 
+uint8_t blank_note_count = 0; 
+
+uint8_t miss_penalty = 1; //miss penalty boolean. Becomes 0 when user catches the correct note.
+uint8_t correct_note; //Correct note that should be pressed. 0->4  for RG0->RG4. -1 when a user should not press any note.
 
 #define TIMER0_PRELOAD_LEVEL1 74
 #define TIMER0_PRELOAD_LEVEL2 4
@@ -54,6 +63,7 @@ uint8_t correct_note;
 
 
 void init_ports() {
+    //IO config
     TRISA = 0x00;
     TRISB = 0x00;
     TRISC = 0x01;
@@ -64,7 +74,7 @@ void init_ports() {
     TRISH = 0x00;
     TRISJ = 0x00;
     
-    
+    //CLRF all
     LATA = 0x00;
     LATB = 0x00;
     LATC = 0x00;
@@ -184,15 +194,18 @@ void init_tmr1() {
 }
 
 
-uint16_t generate_random_note() {
-    uint16_t random_note =  (TMR1H << 8) | TMR1L;
+uint16_t generate_random() {
+    uint16_t random_note ;
     switch(game_level) {
         case 1:
-            random_note = ((random_note >> 1) & 0x0007);
+            random_n_value = (random_n_value >> 1) 
+            random_note = (random_n_value & 0x0007);
         case 2:
-            random_note = ((random_note >> 3) & 0x0007);
+            random_n_value = (random_n_value >> 3) 
+            random_note = (random_n_value & 0x0007);
         case 3:
-            random_note = ((random_note >> 5) & 0x0007);
+            random_n_value = (random_n_value >> 7) 
+            random_note = (random_n_value & 0x0007);
     }
     random_note %= 5;
     return random_note;
@@ -338,6 +351,7 @@ void timer_task() {
         case TMR_RUN:
             break;
         case TMR_DONE:
+            // TODO: Should it become TMR_IDLE just like tmr0 ? 
             break;
     }
 }
@@ -352,15 +366,12 @@ void input_task() {
             game_level = 1;
             health = 9;
             game_over_state = GAME_OVER_DEFAULT;
+            random_n_value = (TMR1H << 8) | TMR1L;
         }
     }
 }
 
-void blank_note_task(){
-    miss_penalty = 1;
-    LATA = 0x00;
-}
-
+//Forwarding previous notes to next notes. 
 void forward_task(){
     LATF = LATD;
     LATD = LATC;
@@ -368,19 +379,22 @@ void forward_task(){
     LATB = LATA;
 }
 
+//Check press task every LATG register with the information of correct note that we assigned in the note_Task(). Same logic for all 4.
 void check_press_task(){
     // LATG0
-    if(LATGbits.LATG0){
+    if(LATGbits.LATG0){ 
         press_state0 = PRSS;
+        if(correct_note == 0) LATF = 0x00;
     }
-    else if(press_state0 == PRSS){
+    else if(press_state0 == PRSS){ // If pressed and released check if it is correct note.
         press_state0 = NOT_PRSS;
-        if(correct_note == 0) {
+        if(correct_note == 0) { //If correct note, no miss_penalty for this note (period) and corresponding RF output pin is gone.
             miss_penalty = 0;
+            LATF = 0x00;
         }
         else {
             
-            if(--health == 0) {
+            if(--health == 0) { // If wrong note, health penalty and game over.
                 game_over_state = GAME_OVER_LOSE;
                 game_over();
             }
@@ -390,6 +404,7 @@ void check_press_task(){
     // LATG1
     if(LATGbits.LATG1){
         press_state1 = PRSS;
+        if(correct_note == 1) LATF = 0x00;
     }
     else if(press_state1 == PRSS){
         press_state1 = NOT_PRSS;
@@ -408,6 +423,7 @@ void check_press_task(){
     // LATG2
     if(LATGbits.LATG2){
         press_state2 = PRSS;
+        if(correct_note == 2) LATF = 0x00;
     }
     else if(press_state2 == PRSS){
         press_state2 = NOT_PRSS;
@@ -426,6 +442,7 @@ void check_press_task(){
     // LATG3
     if(LATGbits.LATG3){
         press_state3 = PRSS;
+        if(correct_note == 3) LATF = 0x00;
     }
     else if(press_state3 == PRSS){
         press_state3 = NOT_PRSS;
@@ -444,6 +461,7 @@ void check_press_task(){
     // LATG4
     if(LATGbits.LATG4){
         press_state4 = PRSS;
+        if(correct_note == 4) LATF = 0x00;
     }
     else if(press_state4 == PRSS){
         press_state4 = NOT_PRSS;
@@ -452,7 +470,7 @@ void check_press_task(){
         }
         else {
             
-            if(--health == 0) {
+            if(--health == 0) { 
                 game_over_state = GAME_OVER_LOSE;
                 game_over();
             }
@@ -460,6 +478,45 @@ void check_press_task(){
     }
 }
 
+//Checks miss note. If note is missed, health -- . 
+void check_miss_task(){
+    if(miss_penalty){
+        if(--health == 0) { // If health is over, health penalty and game over.
+                game_over_state = GAME_OVER_LOSE;
+                game_over();
+            }
+    }
+}
+
+// Gets the correct note according to LATF.
+void get_correct_note(){
+    
+    switch(LATF){
+        case 0x01 :
+            correct_note = 0 ;
+            break;
+        case 0x02 :
+            correct_note = 1 ;
+            break;
+        case 0x04 :
+            correct_note = 2 ;
+            break;
+        case 0x08 :
+            correct_note = 3 ;
+            break;
+        case 0x10 :
+            correct_note = 4 ;
+            break;
+    }
+}
+
+// We light a blank note (do not light anything) for 5 times in order to make the last note reach to last output pins.
+void blank_note_task(){
+    miss_penalty = 1;
+    LATA = 0x00;
+}
+
+// Note task manages the lighting the correct note at the A output pins, assignment of correct_note and miss_penalty. 
 void note_task(){
     uint16_t random_note = generate_random_note();
     
@@ -468,11 +525,11 @@ void note_task(){
         miss_penalty = 0;
     }
     else{
-        correct_note = random_note;
+        get_correct_note(); // correct_note becomes the correct note according to LATF.
         miss_penalty = 1;
     }
     
-    switch(random_note){
+    switch(random_note){ //Lighting the new note. 
         case 0 :
             LATA = 0x01;
             break;
@@ -491,11 +548,14 @@ void note_task(){
     }
 }
 
+//Game task manages levels. When TMR_IDLE for tmr0, note and forwarding tasks are done.
+// When TMR_RUN, press checking is done. When TMR_DONE if a note is missed or not is checked. 
+
 void game_task() {
     if(game_started){
 
         switch(level_state){
-            case LVL_BEG :
+            case LVL_BEG : // No forward task as it is the first note. Note is lighted and press is checking in this state. No need to check miss as the first note is not reached the end.
 
                 switch (tmr0_state){
                     case TMR_IDLE:
@@ -510,7 +570,7 @@ void game_task() {
                 }
                 break;
 
-            case LVL_CONT:
+            case LVL_CONT: // First forwarding, then lighting the new note. If all the notes are lighted,level_state becomes LVL_BLNK. 
                 switch (tmr0_state){
                     case TMR_IDLE:
                         if(note_count != level_max_note){
@@ -528,18 +588,19 @@ void game_task() {
                         check_press_task();
                         break;
                     case TMR_DONE:
+                        check_miss_task();
                         break;
                 }
                 break;
 
-            case LVL_BLANK:
+            case LVL_BLANK: //No new notes (we have blank_notes). If we blanked 5 times, level is done. 
                 switch (tmr0_state){
                     case TMR_IDLE:
                         if(blank_note_count<5){
                             tmr0_startreq = 1; 
                             blank_note_count++;
-                            blank_note_task();
                             forward_task();
+                            blank_note_task();
                         }
                         else{
                             level_state = LVL_END;
@@ -551,11 +612,12 @@ void game_task() {
                         break;
 
                     case TMR_DONE:
+                        check_miss_task();
                         break;
                 }
                 break;
 
-            case LVL_END:
+            case LVL_END: // Checking if we finished the game. If not , next level.
                 blank_note_count = 0 ;
                 note_count = 0; 
                 
